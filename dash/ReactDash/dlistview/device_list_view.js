78e1f4f0d8dc1c39438b2c6059ca033e2d242efc
Daniel Amos Grenehed <daniel.grenehed97@gmail.com>
2022-09-19 13:23:56 +0200

    dash init

---
+++ dash/ReactDash/dlistview/device_list_view.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Ably from 'ably';
+import {Text, View, FlatList} from 'react-native';
+
+const DeviceList = () => {
+    <View>
+        <Text style={Heading}>Connected Devices</Text>
+        <FlatList>
+
+        </FlatList>
+    </View>
+};
